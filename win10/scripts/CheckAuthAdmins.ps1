863d5110315c17f2f9db67e02dde45996780e776
syates21 <samuelyates@gmail.com>
2020-10-10 03:12:47 +0000

    Adding initial user lists and scripts

---
+++ win10/scripts/CheckAuthAdmins.ps1
@@ -0,0 +1,18 @@
+﻿$specialUsers = @("Administrator")
+$authAdmins = Get-Content ..\training\authadmins.txt
+$currentAdmins = Get-LocalGroupMember "Administrators"
+
+# Append the special users to our list of authorized admins
+$authAdmins += $specialUsers
+
+# Get the computer name and prepend it to each entry from the authadmins
+$computerName = $env:COMPUTERNAME
+$authAdmins = $authAdmins | ForEach-Object {"$computerName\$_"}
+
+
+foreach ($currentAdmin in $currentAdmins) {
+  if ($authAdmins -notcontains $currentAdmin) {
+    Write-Output "Removing unauthorized administrator: $currentAdmin)"
+    Remove-LocalGroupMember -Member $currentAdmin -Group "Administrators"
+  }
+}
---
+++ win10/scripts/CheckAuthUsers.ps1
@@ -0,0 +1,16 @@
+﻿$specialusers = @("Administrator", "DefaultAccount", "defaultuser0", "Guest")
+$localusers = Get-LocalUser|select Name
+$authusers = Import-Csv ..\training\authusers.txt
+$authusers += Import-Csv ..\training\authadmins.txt
+
+
+
+foreach ($localuser in $localusers) {
+  if ($authusers.Name -notcontains $localuser.Name) {
+    $unauthUserName = $localuser.Name
+    if ($specialusers -notcontains $unauthUserName) {
+      Write-Output "Removing unauthorized user: $unauthUserName"
+      Remove-LocalUser $unauthUserName
+    }
+  }
+}
